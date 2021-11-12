8af6de11cba219da1fa9e76da84306917e173d07
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2021-11-12 07:41:21 +0500

    adding some more function

--- app.js
+++ app.js
@@ -5,5 +5,22 @@ console.log(deriveExpression("(x*2)+(x^2)"));
 const deviationResult = deriveExpression("(x*2)+(x^2)");
 const Q = 12;
 const result = deviationResult.replace(/x/g, Q);
-console.log(result);
-console.log(result);
+
+// Split using a space character
+let arr = result.split(" ");
+
+// The array
+console.log(arr);
+console.log(arr[0] + arr[1] + arr[2] + arr[3] + arr[4]);
+// console.log(result);
+// console.log(result);
+
+var newReslt = arr.map(function (x) {
+  if (!NaN) {
+    return parseInt(x, 10);
+  } else return x;
+});
+console.log(newReslt);
+console.log(
+  newReslt[0] + newReslt[1] + newReslt[2] + newReslt[3] + newReslt[4]
+);
