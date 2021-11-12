b148394470e078a4b5d93f0cf608fa21964a9ffb
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2021-11-12 11:05:11 +0500

    Adding Functions to allow HTML site to use these

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 import deriveExpression from "./derivation.js";
 
-var expressionValue = "39 + (7*x) - (4*x)^2 + (2*x)^3";
-console.log(deriveExpression(expressionValue));
+function MarginalRevenue(expression, Q) {
+  var expressionValue = expression;
+  console.log(deriveExpression(expressionValue));
 
-var deviationResult = deriveExpression(expressionValue),
-  Q = 9,
-  result = deviationResult.replace(/x/g, Q);
+  var deviationResult = deriveExpression(expressionValue),
+    result = deviationResult.replace(/x/g, Q);
 
-// Split using a space character
-let arr = result.split(" ");
+  // Split using a space character
+  let arr = result.split(" ");
 
-// The array
-// console.log(arr);
-var value1 = [];
-for (let index = 0; index < arr.length; index++) {
-  value1 += `${arr[index]} `;
+  // The array
+  // console.log(arr);
+  var value1 = [];
+  for (let index = 0; index < arr.length; index++) {
+    value1 += `${arr[index]} `;
+  }
+  console.log(value1);
+
+  console.log(eval(value1));
 }
-console.log(value1);
-
-console.log(eval(value1));
