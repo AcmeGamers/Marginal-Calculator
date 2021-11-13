3157bc62053733b4042aa8894a23653ef257031f
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2021-11-13 14:08:12 +0500

    Fixing the function

--- seperate modules/PriceElasticity.js
+++ seperate modules/PriceElasticity.js
@@ -9,36 +9,35 @@ function PriceElasticity(
   // Obtaining the Expression from first Function
 
   console.log(expression);
-  expression.replace(/p1/g, paymentOfGoods1);
-  expression.replace(/p2/g, paymentOfGoods2);
-  expression.replace(/p3/g, paymentOfGoods3);
-  expression.replace(/y/g, income);
+  var AddingValue = expression.replace(/p1/, paymentOfGoods1),
+    AddingValue = AddingValue.replace(/p2/, paymentOfGoods2),
+    AddingValue = AddingValue.replace(/p3/, paymentOfGoods3),
+    AddingValue = AddingValue.replace(/y/, income);
 
-  console.log(expression);
+  console.log(AddingValue);
 
-  function valueCheck(dataExpression) {
-    // Split using a space character
-    let arr = dataExpression.split(" ");
+  // function valueCheck(dataExpression) {
+  // Split using a space character
+  let arr = AddingValue.split(" ");
 
-    // Making the Array
-    var value1 = [];
-    for (let index = 0; index < arr.length; index++) {
-      value1 += `${arr[index]} `;
-    }
-
-    // Result
-    // var finalAnswer = eval(math.evaluate(value1));
-    var finalAnswer = eval(value1);
-    console.log(value1);
-    console.log(finalAnswer);
-
-    answers = {
-      derivative: deriveExpression(expressionValue),
-      puttingValues: value1,
-      answer: finalAnswer,
-    };
-    return answers;
+  // Making the Array
+  var value1 = [];
+  for (let index = 0; index < arr.length; index++) {
+    value1 += `${arr[index]} `;
   }
+
+  // Result
+  // var finalAnswer = eval(math.evaluate(value1));
+  var finalAnswer = eval(value1);
+  console.log(value1);
+  console.log(finalAnswer);
+
+  var answers = {
+    puttingValues: value1,
+    answer: finalAnswer,
+  };
+  //   return answers;
+  // }
   answers = valueCheck(expression);
   // Providing the answers to variables which needs it.
 
@@ -50,7 +49,7 @@ var Expressive = "7777 - P1 + 0.75 * P2 - 0.5*p3 + 0.05 * y",
   p1 = "209",
   p2 = "101",
   p3 = "478",
-  income = "18361",
+  TheIncome = "18361",
   // PriceElasticity(Expressive, p1, p2, p3, income);
   ExpressionResult = Expressive.toLowerCase();
-console.log(PriceElasticity(ExpressionResult, p1, p2, p3, income));
+console.log(PriceElasticity(ExpressionResult, p1, p2, p3, TheIncome));
